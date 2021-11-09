594e1bcc64c08c7f1d85956edb4b834d0d1fbf96
Réda Housni Alaoui <reda-alaoui@hey.com>
2021-11-09 12:25:46 +0100

    JENKINS-67090 Let the build know the reverted change number if there is one
    Change-Id: I45a1cfd9f139b560645cd346bb6694385c647f7d

--- src/main/java/jenkins/plugins/gerrit/GerritEnvironmentContributor.java
+++ src/main/java/jenkins/plugins/gerrit/GerritEnvironmentContributor.java
@@ -125,6 +125,7 @@ public class GerritEnvironmentContributor extends EnvironmentContributor {
     envs.put("GERRIT_BRANCH", change.branch);
     envs.put("GERRIT_TOPIC", nullToEmpty(change.topic));
     envs.put("GERRIT_CHANGE_ID", change.id);
+    envs.put("GERRIT_REVERTED_CHANGE_NUMBER", integerString(change.revertOf));
 
     Map.Entry<String, RevisionInfo> patchSetInfo =
         change
@@ -153,6 +154,10 @@ public class GerritEnvironmentContributor extends EnvironmentContributor {
 
   private String booleanString(Boolean booleanValue) {
     return Optional.ofNullable(booleanValue).orElse(Boolean.FALSE).toString();
+  }
+
+  private String integerString(Integer integerValue) {
+    return Optional.ofNullable(integerValue).map(Object::toString).orElse("");
   }
 
   private String nullToEmpty(String value) {
